7341272209a82e2fbaf4df2f4e244028dca3b48f
hukuhuku <hukuhuku11111arm52@gmail.com>
2025-12-30 20:47:59 +0900

    [add]CrossOriginを一時的に修正

--- backend/src/main/java/com/example/shot_tracker/controller/ShotController.java
+++ backend/src/main/java/com/example/shot_tracker/controller/ShotController.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/shots")
-@CrossOrigin(origins="*")
+@CrossOrigin(origins="http://localhost:5173","https://shot-tracker-theta.vercel.app")
 public class ShotController {
     @Autowired
     private ShotRepository shotRepository;
